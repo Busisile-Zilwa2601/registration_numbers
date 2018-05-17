4cc2281052ca5ae810f70ea124107f4f418d2d6f
Busisile-Zilwa2601 <busisile.bo@gmail.com>
2018-05-17 15:33:14 +0200

    added a reset button to clear the local storange and remove all the li elements

--- js/regstration_numbers-dom.js
+++ js/regstration_numbers-dom.js
@@ -10,24 +10,27 @@ if(localStorage['registration_numbers']){
     objReg = JSON.parse(localStorage.getItem('registration_numbers'));
 }
 function addRegFunction(){
-  var newElement = document.createElement('li');
-  regNumValue = regNum.value;
-  var userSelection = dropDown.options[dropDown.selectedIndex].value;
-  if(reg.enter(regNumValue)){
-    if(reg.size(objReg)>1){
-      objReg = Object.assign(objReg , reg.objtempReg);
+  var newElement = document.createElement('li');//create an empty <li> element
+  regNumValue = regNum.value;//store the value of the user input
+  var userSelection = dropDown.options[dropDown.selectedIndex].value;//store the value of user selection
+  if(reg.enter(regNumValue)){//returns true if the user input is not digits and is one of the cities
+    if(reg.size(objReg)>1 || localStorage['registration_numbers']){//check the size of the object and existance of localStorage
+      objReg = Object.assign(objReg , reg.objtempReg);//we increment the existing object and localStorage
     }
-    else{
-      objReg = reg.objtempReg;
+    else{//empty object and no localStorage
+      objReg = reg.objtempReg;//create new object
     }
     console.log(objReg);
-    var lastElem = reg.last(objReg);
-    if(userSelection === 'capetown'){
-      if(reg.checkCape(lastElem)){
-        newElement.innerHTML = lastElem;
-        list.appendChild(newElement);
-        message.innerHTML = '';
+    var lastElem = reg.last(objReg);//store the last value added on the object
+    if(userSelection === 'capetown'){//check the user selection is cape town
+      if(reg.checkCape(lastElem)){//check the user input is cape town plates
+        newElement.innerHTML = lastElem;//change the li content to be the last value of the object
+        list.appendChild(newElement);//place the li in the ul element, the list
+        message.innerHTML = '';// change the content of the message to empty string.
+        plate.innerHTML = '';
       }
+      /*Else check what the user starts with and place it that that city
+      */
       else{
         lastElem.trim();
         var startString = (lastElem.substring(0,2)).toUpperCase();
@@ -45,12 +48,15 @@ function addRegFunction(){
         }
       }
     }
-    else if(userSelection === 'bellville'){
+    else if(userSelection === 'bellville'){//
       if(reg.checkBell(lastElem)){
         newElement.innerHTML = lastElem;
         list.appendChild(newElement);
         message.innerHTML = '';
+        plate.innerHTML = '';
       }
+      /*Else check what the user starts with and place it that that city
+      */
       else{
         lastElem.trim();
         var startString = (lastElem.substring(0,2)).toUpperCase();
@@ -73,6 +79,7 @@ function addRegFunction(){
         newElement.innerHTML = lastElem;
         list.appendChild(newElement);
         message.innerHTML = '';
+        plate.innerHTML = '';
       }
       else{
         lastElem.trim();
@@ -97,6 +104,7 @@ function addRegFunction(){
         newElement.innerHTML = lastElem;
         list.appendChild(newElement);
         message.innerHTML = '';
+        plate.innerHTML = '';
       }
       else{
         lastElem.trim();
@@ -121,18 +129,22 @@ function addRegFunction(){
       message.innerHTML = '';
     }
   }
+  //The the user input is only digits or is not plate of the cities
   else{
       if(reg.enter(regNumValue) === undefined)
       message.innerHTML = (regNumValue + " invalid");
   }
+  //remove the text on the input
   if(!regNum.value || regNum != regNum.defaultValue){
     regNum.value = regNum.defaultValue;
   }
+  //puting the object on localStorage
   localStorage['registration_numbers'] = JSON.stringify(objReg);
   objReg = JSON.parse(localStorage['registration_numbers']);
 }
 var addRegBtn = document.getElementById('addReg');
 addRegBtn.addEventListener('click', addRegFunction);
+//This function is for printing the plates of the selected town
 function changeSelect(){
   console.log(list.children.length);
   message.innerHTML = '';
@@ -142,48 +154,61 @@ function changeSelect(){
   }
   console.log(list.children);
   if(dropDown.options[dropDown.selectedIndex].value === 'capetown'){
-    if(reg.allCape(reg.objtempReg).length !=0){
-      for(var i = 0; i < reg.allCape(reg.objtempReg).length; i++){
+    if(reg.allCape(objReg).length !=0){
+      for(var i = 0; i < reg.allCape(objReg).length; i++){
         var newElement = document.createElement('li');
-        newElement.innerHTML = reg.allCape(reg.objtempReg)[i];
+        newElement.innerHTML = reg.allCape(objReg)[i];
         list.appendChild(newElement);
       }
     }
   }
   else if(dropDown.options[dropDown.selectedIndex].value === 'bellville'){
-    if(reg.allBellville(reg.objtempReg).length != 0){
-      for(var i = 0; i < reg.allBellville(reg.objtempReg).length; i++){
+    if(reg.allBellville(objReg).length != 0){
+      for(var i = 0; i < reg.allBellville(objReg).length; i++){
         var newElement = document.createElement('li');
-        newElement.innerHTML = reg.allBellville(reg.objtempReg)[i];
+        newElement.innerHTML = reg.allBellville(objReg)[i];
         list.appendChild(newElement);
       }
     }
   }
   else if(dropDown.options[dropDown.selectedIndex].value === 'paarl'){
-    if(reg.allPaarl(reg.objtempReg).length != 0){
-      for(var i = 0; i < reg.allPaarl(reg.objtempReg).length; i++){
+    if(reg.allPaarl(objReg).length != 0){
+      for(var i = 0; i < reg.allPaarl(objReg).length; i++){
         var newElement = document.createElement('li');
-        newElement.innerHTML = reg.allPaarl(reg.objtempReg)[i];
+        newElement.innerHTML = reg.allPaarl(objReg)[i];
         list.appendChild(newElement);
       }
     }
   }
   else if(dropDown.options[dropDown.selectedIndex].value === 'hermanus'){
-    if(reg.allHer(reg.objtempReg).length != 0){
-      for(var i = 0; i < reg.allHer(reg.objtempReg).length; i++){
+    if(reg.allHer(objReg).length != 0){
+      for(var i = 0; i < reg.allHer(objReg).length; i++){
         var newElement = document.createElement('li');
-        newElement.innerHTML = reg.allHer(reg.objtempReg)[i];
+        newElement.innerHTML = reg.allHer(objReg)[i];
         list.appendChild(newElement);
       }
     }
   }
   else{
-    if(reg.size(reg.objtempReg) != 0){
-      for(var i = 0; i < reg.size(reg.objtempReg); i++){
+    if(reg.size(objReg) != 0){
+      for(var i = 0; i < reg.size(objReg); i++){
         var newElement = document.createElement('li');
-        newElement.innerHTML = Object.keys(reg.objtempReg)[i];
+        newElement.innerHTML = Object.keys(objReg)[i];
         list.appendChild(newElement);
       }
     }
   }
 }
+function resetStorage(){
+  if(localStorage['registration_numbers']){
+    localStorage.clear();
+    objReg = new Object();
+    message.innerHTML = '';
+    plate.innerHTML = '';
+    for(var i =0; i = list.children.length ; i){
+        list.removeChild(list.children[0]);
+    }
+  }
+}
+var resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', resetStorage);
